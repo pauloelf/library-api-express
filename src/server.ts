3401327c3ebfdd2d2b53e0156d7d4f914fe7d42a
Paulo Sergio <contato.paulofws@gmail.com>
2026-01-28 20:26:33 -0300

    feat(security): add helmet and cors config

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
+import cors from 'cors'
 import express from 'express'
+import helmet from 'helmet'
 import { errorHandler } from '@/middlewares/errorHandler.js'
 import { jsonSyntaxErrorHandler } from '@/middlewares/jsonSyntaxErrorHandler.js'
 import { requireJson } from '@/middlewares/requireJson.js'
@@ -6,8 +8,17 @@ import routes from '@/routes.js'
 
 const app = express()
 
-app.use(requireJson)
+app.use(helmet())
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(',') ?? ['http://localhost:3333'],
+    credentials: true,
+  }),
+)
+
 app.use(express.json())
+app.use(requireJson)
 
 app.use(routes)
 
